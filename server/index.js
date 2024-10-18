2fa97c1b78e34e4a4a151eb3db7c17bc8388226c
Mahikagarg09 <mg@gwdcollective.com>
2024-10-18 14:20:35 +0530

    setup socket.io

--- server/index.js
+++ server/index.js
@@ -9,6 +9,13 @@ const queryRoute = require("./routes/query");
 const app = express();
 const http = require("http").createServer(app);
 
+// Socket.io instance
+const io = require("socket.io")(http, {
+  cors: {
+    origin: `*`,
+  },
+});
+
 // Constants
 const PORT = 5000;
 const MONGO_URI = "mongodb+srv://mahikagarg23:vA7N2571OUC5EAMJ@cluster0.qtsb9h6.mongodb.net/support-chat";
@@ -30,4 +37,14 @@ mongoose
 // Listen to the connection event
 http.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+// Socket.io connection
+io.on("connection", (socket) => {
+  socket.on("message", ({ userId, message }) => {
+    io.emit("message", { userId, message });
+  });
+  socket.on("resolve", ({ queryId, solution }) => {
+    io.emit("resolve", { queryId, solution });
+  });
 });
