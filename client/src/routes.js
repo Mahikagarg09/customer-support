c6fdbafa0ecb89564bdd8e73caf1c907234c453c
Mahikagarg09 <mg@gwdcollective.com>
2024-10-18 14:20:55 +0530

    routes for client and server connection for queries

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,3 +2,9 @@
 export const host = "http://localhost:5000";
 export const signupRoute = `${host}/api/user/signup`;
 export const loginRoute = `${host}/api/user/login`;
+export const addQuery = `${host}/api/query/add`;
+export const getQueries = `${host}/api/query/all`;
+export const getCurrentCustomerQueries = `${host}/api/query/customer`;
+export const getSlots = `${host}/api/query/slots`;
+export const getAdminQueries = `${host}/api/query/admin`;
+export const resolveQuery = `${host}/api/query/resolve`;
